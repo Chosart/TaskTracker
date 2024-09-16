5e799471d198d9bb28f57a5d670b6f2c9f834552
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-16 21:25:18 +0300

    Added task model

---
+++ TaskTracker/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Priority { get; set; }
+        public DataType CreatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
