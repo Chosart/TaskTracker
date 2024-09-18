8c581d15024c32ffeff31cac39ed1c87619efd77
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-18 20:20:54 +0300

    Added GET method

--- TaskTracker/Controllers/TrackedTaskController.cs
+++ TaskTracker/Controllers/TrackedTaskController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaskTracker.Data;
+using TaskTracker.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TaskTracker.Controllers
 {
@@ -13,6 +15,12 @@ namespace TaskTracker.Controllers
         public TrackedTaskController(TaskTrackerContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TrackedTask>>> GetTrackedTasks()
+        {
+            return await _context.TrackedTasks.ToListAsync();
         }
     }
 }
