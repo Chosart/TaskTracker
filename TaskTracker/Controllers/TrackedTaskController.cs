e4ac4c2baea8fa1abafe07307de0d6976d7f17d1
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-18 20:40:37 +0300

    Added GET by id method

--- TaskTracker/Controllers/TrackedTaskController.cs
+++ TaskTracker/Controllers/TrackedTaskController.cs
@@ -18,9 +18,21 @@ namespace TaskTracker.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TrackedTask>>> GetTrackedTasks()
+        public async Task<ActionResult<TrackedTask>> GetTrackedTasks()
         {
             return await _context.TrackedTasks.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<TrackedTask>>> GetTrackedTasks(int id)
+        {
+            var trackedTasks = await _context.TrackedTasks.FindAsync(id);
+
+            if( trackedTasks == null)
+            {
+                return NotFound();
+            }
+            return Ok(trackedTasks);
         }
     }
 }
