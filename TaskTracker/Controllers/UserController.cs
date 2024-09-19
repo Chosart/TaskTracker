236ffb9c4489a5505fa8599e2bfbedd4cbfe62a3
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-19 10:02:11 +0300

    Initilization UserController ctor

---
+++ TaskTracker/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskTracker.Data;
+using TaskTracker.Models;
+
+namespace TaskTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly TaskTrackerContext _context;
+
+        public UserController(TaskTrackerContext context)
+        {
+            _context = context;
+        }
+    }
+}
