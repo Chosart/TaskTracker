e1dcdd0160be233bfcddda0df222466740e03e18
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-19 10:24:30 +0300

    Added PUT by id method and UserExists method

--- TaskTracker/Controllers/UserController.cs
+++ TaskTracker/Controllers/UserController.cs
@@ -54,5 +54,44 @@ namespace TaskTracker.Controllers
 
             return CreatedAtAction(nameof(GetUser), new {id = user.Id}, user);
         }
+
+        [HttpPut]
+        public async Task<ActionResult<User>> UpdateUser (int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest("ID in the URL does not match the user ID.");
+            }
+           
+            var existingUser = await _context.Users.FindAsync(id);
+            if (existingUser == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            existingUser.UserName = user.UserName;
+            existingUser.Email = user.Email;
+            existingUser.PasswordHash = user.PasswordHash;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!UserExists(id))
+                {
+                    return NotFound("Task not found.");
+                }
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(u => u.Id == id);
+        }
     }
 }
