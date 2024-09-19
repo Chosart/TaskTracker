786d925ff6334590f199963935fada9d2cb59c61
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-19 21:52:34 +0300

    Initilization JWT token in Program.cs

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -1,13 +1,36 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TaskTracker.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["AppSettings:Token"],
+        ValidAudience = builder.Configuration["AppSettings:Token"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]))
+
+
+    };
+});
+
 // Add services to the container.
 builder.Services.AddDbContext<TaskTrackerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TaskTrackerContext")));
-
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
