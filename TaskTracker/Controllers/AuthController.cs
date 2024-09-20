0c2bd5c7a9b792295ff69c3d004f21f9f262b082
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-20 11:42:16 +0300

    Initilization AuthController ctor

---
+++ TaskTracker/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.Data;
+
+namespace TaskTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly TaskTrackerContext _context;
+        private readonly string _tokenKey;
+
+        public AuthController(TaskTrackerContext context, IConfiguration config)
+        {
+            _context = context;
+            _tokenKey = config["AppSettings:Token"];
+        }
+    }
+}
