da7b9f9cfde503cc4cbc6c0b56d62d34facd6b26
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-22 22:10:25 +0300

    Create Tests for TrackedTask and initilization AuthControllerTests and was do little changes

---
+++ TaskTracker.Tests/AuthControllerTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using Moq;
+using TaskTracker.Controllers;
+using TaskTracker.Data;
+using TaskTracker.DTO;
+using TaskTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTracker.Tests
+{
+    public class AuthControllerTests
+    {
+
+        public AuthControllerTests()
+        {
+
+        }
+    }
+}
