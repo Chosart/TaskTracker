b957ab319697253f95f70e7d8d12059f046a649e
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-22 22:33:19 +0300

    Initilisation ctor AuthControllerTests

--- TaskTracker.Tests/AuthControllerTests.cs
+++ TaskTracker.Tests/AuthControllerTests.cs
@@ -5,15 +5,28 @@ using TaskTracker.Data;
 using TaskTracker.DTO;
 using TaskTracker.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace TaskTracker.Tests
 {
     public class AuthControllerTests
     {
-
+        private readonly AuthController _controller;
+        private readonly Mock<TaskTrackerContext> _mockContext;
+        private readonly Mock<IConfiguration> _mockConfig;
         public AuthControllerTests()
         {
+            // Створюємо Mock для TaskTrackerContext
+            _mockContext = new Mock<TaskTrackerContext>();
 
+            // Створюємо Mock для TaskTrackerContext
+            _mockConfig = new Mock<IConfiguration>();
+            _mockConfig.Setup(c => c["AppSettings:Token"]).Returns("TestToken");
+            _mockConfig.Setup(c => c["AppSettings:Issuer"]).Returns("TestIssuer");
+            _mockConfig.Setup(c => c["AppSettings:Audience"]).Returns("TestAudience");
+
+            // Передаємо Mock об'єкти в контролер
+            _controller = new AuthController(_mockContext.Object, _mockConfig.Object);
         }
     }
 }
