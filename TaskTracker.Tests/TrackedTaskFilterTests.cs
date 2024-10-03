2cb860d11f163d06cac50dd3345eab63e93a17f1
Chosart <164737726+Chosart@users.noreply.github.com>
2024-10-03 19:59:43 +0300

    Initilization ctor for TrackedTaskFilterTests

---
+++ TaskTracker.Tests/TrackedTaskFilterTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Controllers;
+using TaskTracker.Data;
+
+namespace TaskTracker.Tests
+{
+    public class TrackedTaskFilterTests
+    {
+        private readonly TrackedTaskController _controller;
+        private readonly TaskTrackerContext _context;
+
+        public TrackedTaskFilterTests()
+        {
+            // Налаштування in-memory бази даних
+            var options = new DbContextOptionsBuilder<TaskTrackerContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new TaskTrackerContext(options);
+            _controller = new TrackedTaskController(_context);
+
+            // Очищення бази даних перед тестами
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+    }
+}
