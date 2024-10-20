54c222835e8561bb8a1acbed7d70177b087100fe
Chosart <164737726+Chosart@users.noreply.github.com>
2024-10-20 21:07:11 +0300

    Initilizations IRedisService and other

---
+++ TaskTracker/Services/IRedisCacheService.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTracker.Services
+{
+    public interface IRedisCacheService
+    {
+        Task SetCacheValueAsync(string key, string value, TimeSpan? expiry = null);
+        Task<string> GetCacheValueAsync(string key);
+        Task RemoveCacheValueAsync(string key);
+        Task<bool> KeyExistAsync(string key);
+    }
+}
