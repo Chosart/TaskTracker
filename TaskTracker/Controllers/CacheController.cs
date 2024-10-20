df2060019d0487342ecec671e0c47b4e5de8aeb3
Chosart <164737726+Chosart@users.noreply.github.com>
2024-10-20 21:42:53 +0300

    Created CacheController and ctor

---
+++ TaskTracker/Controllers/CacheController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.Services;
+
+namespace TaskTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private readonly IRedisCacheService _redisCacheService;
+
+        public CacheController(IRedisCacheService redisCacheService)
+        {
+            _redisCacheService = redisCacheService;
+        }
+    }
+}
